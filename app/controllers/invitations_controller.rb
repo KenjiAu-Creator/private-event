c709fb617ec313e02a5c42fff0cfa7eff28c3ac5
Kenji <kenji.au9@gmail.com>
2020-06-17 12:05:12 -0600

    Fixed the double entry problem for attendence

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -12,11 +12,9 @@ class InvitationsController < ApplicationController
       # Code to save the event into both the event model and the user model
       # as attended_event and Attendee
       flash[:success] = "You have signed up for this event!"
+      # Push the user into the events attendence. The event will know the association through the model.
       @user.attended_events.push(@event)
-      @event.attendees.push(@user)
-      
-      @user.save
-      @event.save
+      # @event.attendees.push(@user)
     else
       flash[:error] = "You are already signed up for this event"
     end
