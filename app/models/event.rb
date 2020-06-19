1ab0799ae34173531ea8a115088b70a10ba90247
Kenji <kenji.au9@gmail.com>
2020-06-19 14:30:56 -0600

    Added in scope method in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+    scope :future, -> (time) {where("created_at > ?", time)}
+    scope :past, -> (time) {where("created_at < ?", time)}
+
     belongs_to :creator, class_name: "User"
     has_many :invitations, foreign_key: "attended_event_id"
     has_many :attendees, through: :invitations, source: :attendee
