b71ff8084badec742b5746c13973a33960302022
Kenji <kenji.au9@gmail.com>
2020-07-28 23:48:22 -0600

    comment out invitation answer

--- db/migrate/20200614004646_change_fields_to_invite.rb
+++ db/migrate/20200614004646_change_fields_to_invite.rb
@@ -2,7 +2,8 @@ class ChangeFieldsToInvite < ActiveRecord::Migration[5.2]
   def change
     add_column :invitations, :attendee_id, :integer
     add_column :invitations, :attended_event_id, :integer
-    remove_column :invitations, :answer
+    # remove_column :invitations, :answer
+
     add_column :invitations, :answer, :boolean
     remove_column :invitations, :guest_id
     remove_column :invitations, :host_id
